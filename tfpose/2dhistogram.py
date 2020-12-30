90a21037056af487157b4c004d3af86ced498d8c
WoodoLee <woodo.lee@gmail.com>
2020-12-30 14:48:15 +0900

    wdlee

--- tfpose/2dhistogram.py
+++ tfpose/2dhistogram.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib
+import plotly.express as px
 
 parser = argparse.ArgumentParser(description='for preprocessing tfpose data...')
 parser.add_argument('--file', type=str, required=True, help='데이터 경로')
@@ -15,12 +16,21 @@ df = pd.read_pickle(args.file)      # x, y, score
 body = ["Nos", "Nec", "Rsh", "Rel", "Rwr", "Lsh", "Lel", "Lwr", "Rey", "Ley", "Rea", "Lea"]
 
 for i in body:
+    
     XY = pd.concat([df[i + '_X'], df[i + '_Y'], df[i + '_Score']], axis=1)
     XY.columns = ['X', 'Y', 'Score']
     histo = histo.append(XY)
 
+histo = histo[histo.X != 0]
+histo = histo[histo.Y != 0]
+
+print(histo)
+
+fig = px.density_heatmap(histo, x="X", y="Y", marginal_x="histogram", marginal_y="histogram",nbinsx=150, nbinsy=150)
+fig.show()
 
 
+"""
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 hist, xedges, yedges = np.histogram2d(histo['X'], histo['Y'], bins=50)
@@ -41,5 +51,5 @@ dz = hist.flatten()
 ax.bar3d(xpos, ypos, zpos, dx, dy, dz, color='b', zsort='average')
 
 plt.show()
-
+"""
 # python3 2dhistogram.py --file Desktop/tfpose/hci_tfpose/data_pickle/kjk_C03_1.pkl
