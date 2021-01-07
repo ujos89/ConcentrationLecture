2ab57c8c236ac0186394b13f2cb9c6f4fa8367de
WoodoLee <woodo.lee@gmail.com>
2021-01-07 21:24:20 +0900

    wdlee

--- tfpose/analysis/anal.py
+++ tfpose/analysis/anal.py
@@ -7,44 +7,23 @@ parser.add_argument('--path', type=str, required=True, help='pickle file to read
 args = parser.parse_args()
 
 dfRaw = pd.read_pickle(args.path)
-print(dfRaw)
 
-nBins= 25
+nBins= 100
+rangeMin = 0
+rangeMax = 1
 
-plt.subplot(3,2,1)
-plt.hist(dfRaw.loc[lambda df: df['label(1-fold)']==0, 'predcition(1-fold)'], bins=nBins,density=True, alpha=0.5, histtype='step' )
-plt.hist(dfRaw.loc[lambda df: df['label(1-fold)']==1, 'predcition(1-fold)'], bins=nBins,density=True, alpha=0.5, histtype='step' )
-plt.xlabel('Prediction Value')
-plt.ylabel('Numbers')
-plt.xlim([0, 1])  
+dfAll = pd.DataFrame()
 
-plt.subplot(3,2,2)
-plt.hist(dfRaw.loc[lambda df: df['label(2-fold)']==0, 'predcition(2-fold)'], bins=nBins,density=True, alpha=0.5, histtype='step' )
-plt.hist(dfRaw.loc[lambda df: df['label(2-fold)']==1, 'predcition(2-fold)'], bins=nBins,density=True, alpha=0.5, histtype='step' )
-plt.xlabel('Prediction Value')
-plt.ylabel('Numbers')
-plt.xlim([0, 1])  
+for i in range(1,6):
+    namePre = 'predcition(' + str(i) + '-fold)'
+    nameLabel = 'label(' + str(i) + '-fold)'
+    dfTemp = dfRaw[[namePre, nameLabel]]
+    dfTemp.columns=['prediction','label']
+    dfAll = pd.concat([dfAll, dfTemp])
 
-plt.subplot(3,2,3)
-plt.hist(dfRaw.loc[lambda df: df['label(3-fold)']==0, 'predcition(3-fold)'], bins=nBins,density=True, alpha=0.5, histtype='step' )
-plt.hist(dfRaw.loc[lambda df: df['label(3-fold)']==1, 'predcition(3-fold)'], bins=nBins,density=True, alpha=0.5, histtype='step' )
-plt.xlabel('Prediction Value')
-plt.ylabel('Numbers')
-plt.xlim([0, 1])  
+df1_0 = dfRaw.loc[lambda df: df['label(1-fold)']==0 , lambda df : ['predcition(1-fold)', 'label(1-fold)']]
+df1_1 = dfRaw.loc[lambda df: df['label(1-fold)']==1 , lambda df : ['predcition(1-fold)', 'label(1-fold)']]
 
-plt.subplot(3,2,4)
-plt.hist(dfRaw.loc[lambda df: df['label(4-fold)']==0, 'predcition(4-fold)'], bins=nBins,density=True, alpha=0.5, histtype='step' )
-plt.hist(dfRaw.loc[lambda df: df['label(4-fold)']==1, 'predcition(4-fold)'], bins=nBins,density=True, alpha=0.5, histtype='step' )
-plt.xlabel('Prediction Value')
-plt.ylabel('Numbers')
-plt.xlim([0, 1])  
-
-plt.subplot(3,2,5)
-plt.hist(dfRaw.loc[lambda df: df['label(5-fold)']==0, 'predcition(5-fold)'], bins=nBins,density=True, alpha=0.5, histtype='step' )
-plt.hist(dfRaw.loc[lambda df: df['label(5-fold)']==1, 'predcition(5-fold)'], bins=nBins,density=True, alpha=0.5, histtype='step' )
-plt.xlabel('Prediction Value')
-plt.ylabel('Numbers')
-plt.xlim([0, 1])  
-
-plt.show()
+print(df1_0)
+print(df1_1)
 
