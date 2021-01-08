91dd828f0e5f1b6b28cac3fb6d8a9c1a2d964642
Woodo LEE <wdlee@Woodoui-MacBookPro.local>
2021-01-08 16:26:07 +0900

    cleanup

--- tfpose/read_pickle.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import argparse
-
-parser = argparse.ArgumentParser(description='for preprocessing tfpose data...')
-parser.add_argument('--path', type=str, required=True, help='pickle file to read')
-args = parser.parse_args()
-
-df = pd.read_pickle(args.path)
-print(df)
